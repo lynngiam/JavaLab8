b7f63bfca2b9efd4c48dcc38cb0788db81e556b0
lamchiminhmark <34977592+lamchiminhmark@users.noreply.github.com>
2018-04-22 17:44:29 -0400

    Update MyPriorityQueueTest.java

--- lab8/MyPriorityQueueTest.java
+++ lab8/MyPriorityQueueTest.java
@@ -17,51 +17,73 @@ public class MyPriorityQueueTest {
     public void testAdd() {
 	MyPriorityQueue<String> test = new MyPriorityQueue<String>(new StringComparator());
 	PriorityQueue<String> standard = new PriorityQueue<String>(10, Collections.reverseOrder());
-	test.add("A");
-	test.add("B");
 	test.add("C");
+	test.add("B");
+	test.add("A");
 	test.add("D");
 	test.add("E");
-	standard.add("A");
-	standard.add("B");
 	standard.add("C");
+	standard.add("B");
+	standard.add("A");
 	standard.add("D");
 	standard.add("E");
 	assertEquals("Size: ", test.size(), standard.size());
 	assertEquals("ToString: ", standard.toString(), test.toString());
-
+	
+	
     }
 
     @Test
     public void testRemove() {
 	MyPriorityQueue<String> test = new MyPriorityQueue<String>(new StringComparator());
 	PriorityQueue<String> standard = new PriorityQueue<String>(10, Collections.reverseOrder());
-	test.add("A");
-	test.add("B");
 	test.add("C");
+	test.add("B");
+	test.add("A");
 	test.add("D");
 	test.add("E");
-	standard.add("A");
-	standard.add("B");
 	standard.add("C");
+	standard.add("B");
+	standard.add("A");
 	standard.add("D");
 	standard.add("E");
-	assertEquals("Size: ", test.size(), standard.size());
-	for (int i = 0; i < test.size(); i++) {
-	    System.out.println(test.remove());
-	    // assertEquals("After add: ", standard.poll(), test.remove());
+	while (test.size() != 0) {
+		System.out.print(test.size() + " ");
+		System.out.print(test.remove() + " ");
+		System.out.println();
 	}
 
     }
 
     @Test
     public void testIsEmpty() {
-	fail("Not yet implemented");
+    	MyPriorityQueue<String> test = new MyPriorityQueue<String>(new StringComparator());
+    	test.add("C");
+    	test.add("B");
+    	test.add("A");
+    	test.add("D");
+    	test.add("E");
+    	assertFalse("Priority queue is empty!", test.isEmpty());
+    	test.clear();
+    	assertTrue("Priority queue is not empty!", test.isEmpty());
     }
 
     @Test
     public void testSize() {
-	fail("Not yet implemented");
+    	MyPriorityQueue<String> test = new MyPriorityQueue<String>(new StringComparator());
+    	PriorityQueue<String> standard = new PriorityQueue<String>(10, Collections.reverseOrder());
+    	test.add("C");
+    	test.add("B");
+    	test.add("A");
+    	test.add("D");
+    	test.add("E");
+    	standard.add("C");
+    	standard.add("B");
+    	standard.add("A");
+    	standard.add("D");
+    	standard.add("E");
+    	assertEquals("The size of the priority queue is: ", standard.size(), test.size());
+
     }
 
     @Test
@@ -71,7 +93,19 @@ public class MyPriorityQueueTest {
 
     @Test
     public void testToString() {
-	fail("Not yet implemented");
+    	MyPriorityQueue<String> test = new MyPriorityQueue<String>(new StringComparator());
+    	PriorityQueue<String> standard = new PriorityQueue<String>(10, Collections.reverseOrder());
+    	test.add("C");
+    	test.add("B");
+    	test.add("A");
+    	test.add("D");
+    	test.add("E");
+    	standard.add("C");
+    	standard.add("B");
+    	standard.add("A");
+    	standard.add("D");
+    	standard.add("E");
+    	assertEquals("The size of the priority queue is: ", standard.toString(), test.toString());
     }
 
 }
